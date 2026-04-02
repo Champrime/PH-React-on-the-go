47bd588a721ead3dc2ee990fff4f596e1f8cb49a
Shakawat Hossain Sadik <saadiq9x@gmail.com>
2026-04-02 08:19:07 +0600

    Continuous Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Suspense } from 'react'
+import './App.css';
+import Countries from './components/countries/Countries';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>React world on the go...</h1>
+      <Suspense fallback={<h6>Loading...</h6>}>
+        <Countries></Countries>
+      </Suspense>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,24 @@
+import React, {use} from 'react';
+import Country from "../Country/Country.jsx"
+
+const CountriesAPI = async() => {
+    const xCountries = await fetch("https://openapi.programming-hero.com/api/all");
+    const res = await xCountries.json();
+    return res;
+}
+
+const countriesPromise = CountriesAPI();
+
+const Countries = () => {
+    const {countries} = use(countriesPromise);
+    console.log(countries);
+    return (
+        <div>
+            <h1>In {countries.length} countries</h1>
+            {Countries.map(c => <Country country={c}></Country>
+            )}
+        </div>
+    );
+};
+
+export default Countries;
