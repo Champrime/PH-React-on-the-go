9230c8ba2633bcbc0ab51bb5a5f8cacf9b1e62f5
Shakawat Hossain Sadik <saadiq9x@gmail.com>
2026-04-03 04:43:00 +0600

    Continuous Commit

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,35 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
-export default function Country({country}){
-    const {name, capital, flags, population} = country;
+export default function Country({visitedOrNot, flagSeenOrNot, country}){
+    const {name, flags, population, area} = country;
     const {common, official} = name;
-    const {png, svg, alt} = flags.flags;
+    const {png, alt} = flags.flags;
+    const [visited, setVisited] = useState(false);
+
+    const handleClick = () => {
+        setVisited(!visited);
+        visitedOrNot(country);        
+        console.log(country);
+    }
+
+    const handleFlag = (flags) => {
+        flagSeenOrNot(flags);
+    }
 
     return(
         <>
             <Suspense fallback = { <p>data of {official} is loading</p> }>
-                <h4> <img src={png} alt={alt} /> <span>{common}</span></h4>
-                <ul>
-                    <li>{population.population}</li>
-                </ul>
+                <div className={`flex flex-col gap-5 p-4 ${visited ? "text-amber-600 bg-amber-100 border border-amber-300" : "text-amber-900 bg-amber-400 border border-amber-100"} rounded-xl`}>
+                    <h4 className="flex flex-col md:flex-row gap-1.5 items-center font-extrabold text-2xl"><img src={png} alt={alt} className="h-6 w-10"/><span>{common}</span></h4>
+                    <ul>
+                        <li className="list-none pl-2.5">Area: {area?.area} ({area?.area > 30000? "Big Country" : "Small Country"})</li>
+                        <li className="list-none pl-2.5">Population: {population?.population}</li>
+                    </ul>
+                    <div className="flex justify-around">
+                        <button onClick={handleClick} className="btn btn-soft btn-warning">{visited ? "Visited" : "Not Visited"}</button>
+                        <button onClick={()=>{handleFlag(flags.flags)}} className="btn btn-soft btn-warning">Add visited country flag</button>
+                    </div>
+                </div>
             </Suspense>
         </>
     )
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,5 +1,7 @@
-import React, {use} from 'react';
-import Country from "../Country/Country.jsx"
+import React, {use, useState} from 'react';
+import Country from "../Country/Country.jsx";
+
+// setVisitedCountries(v => visitedCountries.append(v));   
 
 const CountriesAPI = async() => {
     const xCountries = await fetch("https://openapi.programming-hero.com/api/all");
@@ -9,14 +11,39 @@ const CountriesAPI = async() => {
 
 const countriesPromise = CountriesAPI();
 
+
 const Countries = () => {
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [countryFlags, setCountryFlags] = useState([]);
+    const handleVisitedCountries = (country) => {
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+        // setVisitedCountries(v => console.log(v));
+        // setVisitedCountries(v => v.push(x));
+        // console.log(visitedCountries?.length);
+        // visited? setVisitedCountries(x => x.push(common)) : setVisitedCountries(x => x.filter(x => x === !visited));
+    };
+    const handleCountryFlags = (flags) => {
+        const newCountryFlags = [...countryFlags, flags];
+        setCountryFlags(newCountryFlags);
+    }
     const {countries} = use(countriesPromise);
-    console.log(countries);
+    // console.log(countries);
     return (
         <div>
             <h1>In {countries.length} countries</h1>
-            {countries.map(c => <Country key={c?.cca3?.cca3} country={c}></Country>
-            )}
+            <h3>{visitedCountries.length} countries visited so far</h3>
+            <ul className="flex items-center gap-4 p-4">
+            {
+                visitedCountries.map(x => <li key={x?.cca3?.cca3}>{x.name.common}</li> )
+            }
+            {
+                countryFlags.map((x, index) => <li key={index}><img src={x.png} alt={x.alt} className="w-5 h-3"/></li>)
+            }
+            </ul>
+            <div className="grid grid-cols-3 gap-4.5">
+                {countries.map(c => <Country key={c?.cca3?.cca3} visitedOrNot={handleVisitedCountries} flagSeenOrNot = {handleCountryFlags} country={c}></Country>)}
+            </div>
         </div>
     );
 };
